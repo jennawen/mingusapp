19ea44d43e9fea6fff3cc32193129d67648a4448
Jenny Yee <jennylyee@gmail.com>
2013-12-07 22:16:58 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mingusapp::Application.config.secret_key_base = '59e0a636b4c1d4d8ebc3ac222198b83e790c7eb25d78cddd9338562e0bd11ffdaaace7912307ef6f11d26baf2cfdce94a61330eeece0162ab14603341ba08ea4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mingusapp::Application.config.session_store :cookie_store, key: '_mingusapp_session'
